dbb895b33ecd1203498d59bc7593594ac18782d6
Botan <botan.david94@gmail.com>
2020-05-22 15:59:35 +0200

    Reformat files + separate and cleanup python file in multiple file

--- project/app/test-linear-classification/main.py
+++ project/app/test-linear-classification/main.py
@@ -14,7 +14,9 @@ img_total_size = (img_size * img_size * img_channel)
 LIB_PATH = os.path.abspath("..\\..\\lib\\cmake-build-debug\\ML-framework.dll")
 train_percent = 0.85
 
-liveTest = True
+live_test = True
+force_train = False
+test_dataset = False
 
 networks = {}
 
@@ -28,12 +30,28 @@ ml_lib = load_ml_library(LIB_PATH, img_total_size, len(XFlattened), len(Y))
 
 print("Successfully loaded ML library")
 
+
+def analyse_dataset():
+    correct = 0
+    for i in range(0, len(X_test)):
+        data = X_test[i]
+        must_predict = labels[Y_test[i]]
+        (predictions, best_match) = predict_face(ml_lib, networks, data)
+        if best_match == must_predict:
+            correct += 1
+        else:
+            plt.imshow(np.reshape(data, (img_size, img_size, img_channel)))
+            plt.show()
+            print('Mismatch', predictions, must_predict)
+    return correct / len(Y_test) * 100
+
+
 for label in labels:
     save_path_model = os.path.abspath("..\\..\\models\\face-classification\\" + label + ".model")
     labelId = labels.index(label)
     model = None
 
-    if os.path.exists(save_path_model):
+    if os.path.exists(save_path_model) and not force_train:
         print('Restore neural network for label', label, ' id = ', labelId)
         model = ml_lib.loadModel(c_char_p(save_path_model.encode()))
 
@@ -54,21 +72,11 @@ for label in labels:
 
     networks[label] = model
 
-correct = 0
-for i in range(0, len(X_test)):
-    data = X_test[i]
-    must_predict = labels[Y_test[i]]
-    (predictions, best_match) = predict_face(ml_lib, networks, data)
-    if best_match == must_predict:
-        correct += 1
-    else:
-        plt.imshow(np.reshape(data, (48, 48, 3)))
-        plt.show()
-        print('Mismatch', predictions, must_predict)
+if test_dataset:
+    accuracy = analyse_dataset()
+    print('Neural network accuracy:', accuracy)
 
-print('Accuracy = ', (correct / len(Y_test) * 100), ' -- ', correct, '/', len(Y_test))
-
-if liveTest:
+if live_test:
     start_camera_stream(ml_lib, networks, img_total_size)
 
 for network in networks.values():
